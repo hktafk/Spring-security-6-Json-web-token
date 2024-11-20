94608d8ef23c1add3a8978e1f6624d719e6977e5
hktafk <ngoduclochb123@gmail.com>
2024-11-20 07:52:41 +0700

    Added my project

---
+++ src/main/java/vn/iotstar/demo_sitemesh_thymeleaf/controllers/web/HomeController.java
@@ -0,0 +1,17 @@
+package vn.iotstar.demo_sitemesh_thymeleaf.controllers.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        return "web/home";
+    }
+    
+    @GetMapping("/web/home")
+    public String webHome() {
+        return "web/home"; 
+    }
+}
